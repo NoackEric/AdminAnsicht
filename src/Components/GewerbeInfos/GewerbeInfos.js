0edc3069a0b728b0e1f13ff1eb665dcb9d0f5cbc
Eric Noack <ericnoack91@gmail.com>
2019-11-29 14:21:34 +0100

    Gewerbe Info hinzugefügt h2 padding entfernt

--- src/Components/GewerbeInfos/GewerbeInfos.js
+++ src/Components/GewerbeInfos/GewerbeInfos.js
@@ -6,7 +6,7 @@ const Gewerbe = () => {
             <section className='gewerbeSection'>
                 <h2>Bitte schreiben sie in kurzen Sätzen</h2>
                 <form className='gewerbeForm' action="#">
-                    <p>Wobei handelt es sich, bei Ihrem neuen Projekt?</p>
+                    <p>Worum handelt es sich bei Ihrem neuen Projekt?</p>
                     <textarea rows="4" size='50' cols="50" name="comment" form="usrform" placeholder='z.B Neuentwicklung, Überarbeitung?' />
                     <p>Wenn sie erlauben, welches Ziel steuert Ihr neues Projekt an?</p>
                     <textarea rows="4" size='50' cols="50" name="comment" form="usrform" placeholder='z.B Informieren, Verkaufen, Kundengewinnung ...?' />
