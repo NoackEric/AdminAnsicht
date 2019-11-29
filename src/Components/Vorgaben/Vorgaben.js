3220c2fd800c2283067e0736a9fa9dd580096022
Eric Noack <ericnoack91@gmail.com>
2019-11-29 17:44:39 +0100

    Vorgaben Component

--- src/Components/Vorgaben/Vorgaben.js
+++ src/Components/Vorgaben/Vorgaben.js
@@ -12,7 +12,18 @@ const Vorgaben = () => {
                 <input type="file" enctype="multipart/form-data" />
                 <p>Was ist ein Must have auf Ihrer Seite?</p>
                 <input className='mustHave' type="text" placeholder='Farben, Logos, ...?' size='50' /> <br />
-                <input className='VorgabenSectionSubmit' type="submit" value="Zur vorletzten Seite"/>
+                <p>Gibt es noch bestimmte Technologien die zum entstehen <br />
+                    der Seite verwendet werden müssen?
+                </p>
+                <button>HTML/CSS</button>
+                <button>Javascript</button>
+                <button>React</button>
+                <button>Php</button>
+                <button>Node</button>
+                <button>Vue.js</button>
+                <input className='mustHave' type="text" placeholder='Andere Sprachen' size='50' /> <br />
+                <br />
+                <input className='VorgabenSectionSubmit' type="submit" value="Zur vorletzten Seite" />
             </form>
         </section>
     );
