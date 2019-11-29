e056534018d5953de6b4d4ce7e42e10076d1dc64
Eric Noack <ericnoack91@gmail.com>
2019-11-29 09:38:16 +0100

    briefing hinzugefügt

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import Admin from './Components/Admin/AdminAnsicht';
 import Anrede from './Components/Anrede/Anrede';
+import Antrag from './Components/NeuBriefing/Antrag';
 
 
 
 function App() {
   return (
     <div className="App">
-      <Anrede />
-   
+      <Antrag />
     </div>
   );
 }
---
+++ src/Components/NeuBriefing/Antrag.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './antrag.css'
+
+const Antrag = () => {
+    return (
+        <section className='newBriefing'>
+                <h2>Erstelle jetzt ein Briefing für deinen Auftrag</h2>
+                <p>Du kannst hier in einigen schritten ganz einfach dein eigenes Projekt 
+                    briefen. <br />
+                    Dazu brauchen wir einige angaben von dir,
+                    im Optimal fall hast du auch schon ein 
+                    Mock up für uns.
+                    <br /> <br />
+                    Viel spaß beim Briefing!
+                </p>
+                <button>Neues Briefing</button>
+            
+        </section>
+    );
+}
+
+export default Antrag;
